ab2005e20e04507808db4ac88780b149a719f2c3
sery270 <sery270@naver.com>
2020-08-11 23:47:05 +0900

    [BOJ] 10809_알파벳찾기.cpp

---
+++ DataStructure/[BOJ] 10809_알파벳찾기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+int alpha[27];
+int main() {
+    string input;
+    cin >> input;
+    for (int i = 0; i < 26; i++)
+        alpha[i] = -1;
+    for (int i = 0; i < input.length(); i++)
+        if(alpha[input[i] - 97] == -1)
+            alpha[input[i] - 97] = i;
+    for (int j = 0; j < 26; j++)
+        cout << alpha[j] << " ";
+    return 0;
+}
+
+
+출처: https://min92.tistory.com/51 [A Common Programmer]
