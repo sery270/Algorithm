c1b3dd218fe9623bef1824d1c24dd995627116ac
sery270 <sery270@naver.com>
2020-08-18 22:44:50 +0900

    [BOJ] 11726_2XN 타일링

---
+++ DP/[BOJ] 11726_2XN 타일링.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+vector<long long> mem(1001);
+
+int N;
+long long sol;
+
+long long dp(int n){
+//    int m1, m2;
+    if(mem[n] > 0){ // 값이 있을 때
+        return mem[n];
+    }else{
+        mem[n] = dp(n-2)%10007 + dp(n-1)%10007;
+    }
+    
+    return mem[n];
+}
+
+int main(){
+    scanf("%d",&N);
+    mem[0] = 1;
+    mem[1] = 1;
+    mem[2] = 2;
+    if(N == 1){
+        sol = 1;
+    }else if(N == 2){
+        sol = 2;
+    }else{
+        sol = dp(N);
+    }
+    //int s = sol;
+    printf("%d",sol % 10007);
+    
+    
+    
+    
+    return 0;
+}
+
