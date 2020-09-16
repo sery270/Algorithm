bbaa9da1d585bf32f6344993fa415f88f4e4ad29
sery270 <sery270@naver.com>
2020-09-16 23:29:05 +0900

    [BOJ] 1152_단어의 개수

---
+++ DataStructure/[BOJ] 1152_단어의 개수.py
@@ -0,0 +1,11 @@
+
+# n = sys.stdin.readline()
+print(len(input().split()))
+
+# for i in range(10):
+#     print(i)
+# l = [ 0 for _ in range(10)]
+# l = [int(input()) for _ in range(n)]
+# for idx,val in enumerate(l):
+
+
