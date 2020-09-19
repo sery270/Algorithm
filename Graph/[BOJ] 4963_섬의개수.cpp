fd36c40c8bf737495d2ae1af11928b7701bb1326
sery270 <sery270@naver.com>
2020-09-19 17:02:52 +0900

    [BOJ] 4963_섬의개수 :: 최적화

--- Graph/[BOJ] 4963_섬의개수.cpp
+++ Graph/[BOJ] 4963_섬의개수.cpp
@@ -8,15 +8,35 @@ int dy[8] = {-1,1,0,0,-1,1,-1,1};
 int W, H, tmp;
 int cnt = 0;
 
-void dfs(vector<vector<int>> tmp, int x, int y){
-    if(tmp[x][y] == 0){
+// 배열을 함수의 인자로 주고 받는것은 메모리든 시간이든 매우 비효율 적이다. 
+// 전역 배열을 선언하는 것이 PS에선 좋겠다. 
+
+// void dfs(vector<vector<int>> tmp, int x, int y){
+//     if(tmp[x][y] == 0){
+//         return;
+//     }
+    
+//     g[x][y] = 0;
+    
+//     for (int i =0; i < 8; i++) {
+//         dfs(g, x+dx[i], y+dy[i]);
+//          //cout << "dfs>>>" << i << "x: " << x << "y: " << y << endl;
+//     }
+// }
+
+void dfs(int x, int y){
+    if(g[x][y] == 0){
         return;
     }
-    
+
     g[x][y] = 0;
     
     for (int i =0; i < 8; i++) {
-        dfs(g, x+dx[i], y+dy[i]);
+        //이렇게 재귀 호출이 필요한 경우를 필터링 해주는 것은 시간적으로 많이 절약된다. 
+        //메모리는 큰 상관이 없다.
+        if(g[x+dx[i]][y+dy[i]] == 1){
+            dfs(x+dx[i], y+dy[i]);
+        }
          //cout << "dfs>>>" << i << "x: " << x << "y: " << y << endl;
     }
 }
@@ -30,6 +50,7 @@ int main(){
     while(W != 0 && H != 0){
         
         cnt = 0;
+        //resize는 시간이든, 메모리든 큰 영향이 없다.
         g.resize(52,vector<int>(52));
         for(int i = 1; i<= H; i++){
             for(int j = 1; j<= W; j++){
@@ -41,7 +62,8 @@ int main(){
             for(int j = 1; j<= W; j++){
                 if(g[i][j] == 1){
                     //cout << "x: " << i << "y: " << j << endl;
-                    dfs(g, i, j);
+                    //dfs(g, i, j);
+                    dfs(i, j);
                     cnt++;
                 }
             }
