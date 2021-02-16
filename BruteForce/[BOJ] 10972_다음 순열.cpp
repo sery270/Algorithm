f5c9262433a9e99790a750fcd9452a19ad3cc241
sery270 <sery270@naver.com>
2021-02-16 20:29:10 +0900

    [BOJ] 10972_다음 순열 (STL 사용)

--- BruteForce/[BOJ] 10972_다음 순열.cpp
+++ BruteForce/[BOJ] 10972_다음 순열.cpp
@@ -1,61 +1,87 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int N, tmp;
+// vector <int> v;
+
+// void input(){
+//     cin >> N;
+//     v.resize(N);
+//     for(int i =0; i<N; i++){
+//         cin >> tmp;
+//         v[i] = tmp;
+//     }
+    
+// }
+
+// bool next_p(vector<int> &a, int n){
+//     int i , j;
+    
+//     i = N-1;
+//     while(i > 0 && v[i-1] >= v[i]){
+//         i -= 1;
+//     }
+//     if(i <=0){
+//         return false;
+//     }
+    
+//     j = N-1;
+//     while(v[j] <= v[i-1]){
+//         j -= 1;
+//     }
+//     swap(v[i-1], v[j]);
+//     j = N-1;
+    
+//     while(i < j){
+//         swap(v[i], v[j]);
+//         i += 1;
+//         j -= 1;
+//     }
+    
+//     return true;
+    
+// }
+
+
+// int main(){
+//     input();
+    
+//     if(next_p(v, N)){
+//         for(int i = 0; i<N; i++){
+//             cout << v[i] << ' ';
+//         }
+//     }else{
+//         cout << "-1";
+//     }
+    
+    
+    
+//     return 0;
+// }
+#include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm>
 using namespace std;
 
-int N, tmp;
-vector <int> v;
-
-void input(){
-    cin >> N;
-    v.resize(N);
-    for(int i =0; i<N; i++){
-        cin >> tmp;
-        v[i] = tmp;
-    }
-    
-}
-
-bool next_p(vector<int> &a, int n){
-    int i , j;
-    
-    i = N-1;
-    while(i > 0 && v[i-1] >= v[i]){
-        i -= 1;
-    }
-    if(i <=0){
-        return false;
-    }
-    
-    j = N-1;
-    while(v[j] <= v[i-1]){
-        j -= 1;
-    }
-    swap(v[i-1], v[j]);
-    j = N-1;
-    
-    while(i < j){
-        swap(v[i], v[j]);
-        i += 1;
-        j -= 1;
-    }
-    
-    return true;
-    
-}
-
-
 int main(){
-    input();
+    int N;
+    scanf("%d", &N);
+    vector<int> list(N);
+    for(int i = 0; i<N; i++){
+        scanf("%d", &list[i]);
+    }
     
-    if(next_p(v, N)){
-        for(int i = 0; i<N; i++){
-            cout << v[i] << ' ';
+    if(next_permutation(list.begin(), list.end())){
+        for(int i =0; i<N; i++){
+            printf("%d ", list[i]);
         }
     }else{
-        cout << "-1";
+        printf("-1");
     }
-    
-    
-    
+
     return 0;
+    
 }
