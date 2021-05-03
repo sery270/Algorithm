a35a5d3065b935c43b186d9ea78d69220b0948aa
sery270 <sery270@naver.com>
2021-05-03 20:02:12 +0900

    [PG] 신규아이디추천 refactor

--- Simulation/[PG] 신규아이디추천.py
+++ Simulation/[PG] 신규아이디추천.py
@@ -1,25 +1,47 @@
+
 def solution(new_id):
-    # 1단계
-    new_id = new_id.lower()
-    # 2단계
     answer = ''
-    for word in new_id:
-        if word.isalnum() or word in '-_.':
-            answer += word
-    # 3단계
-    while '..' in answer:
-        answer = answer.replace('..', '.')
-    # 4단계
-    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
-    answer = answer[:-1] if answer[-1] == '.' else answer
-    # 5단계
-    answer = 'a' if answer == '' else answer
-    # 6단계
+    # 1
+    new_id = new_id.lower()
+    
+    # 2
+    for char in new_id :
+        if char.isalnum() or char in '_-.':
+            answer += char
+    
+    # 3
+    while True:
+        if ".." in answer:
+            answer = answer.replace("..", ".")
+        else:
+            break
+    
+    # 5
+    if len(answer) == 0: 
+        answer = "a"
+    else : # 4
+        if answer[0] == '.':
+            if len(answer) == 1 :
+                answer = "a"
+            else:
+                answer = answer[1:]
+        if answer[-1] == '.':
+            if len(answer) == 1 :
+                answer = "a"
+            else:
+                answer = answer[:-1]
+    
+    
+    # 6
     if len(answer) >= 16:
         answer = answer[:15]
         if answer[-1] == '.':
             answer = answer[:-1]
-    # 7단계
-    if len(answer) <= 3:
-        answer = answer + answer[-1] * (3-len(answer))
+
+    # 7 
+    tmp = answer[-1]
+    while len(answer) < 3:
+        answer += tmp
+    
     return answer
+
